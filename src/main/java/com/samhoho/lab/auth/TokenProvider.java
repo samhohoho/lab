d8bfa35402db603a9912c8e7cc3922a1521428b2
samhohoho <hosamchung998@gmail.com>
2024-09-02 11:41:50 +0800

    store secret key in .env and use

--- src/main/java/com/samhoho/lab/auth/TokenProvider.java
+++ src/main/java/com/samhoho/lab/auth/TokenProvider.java
@@ -1,20 +1,27 @@
 package com.samhoho.lab.auth;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class TokenProvider {
-    private String jwtSecret;
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
 
-    private Key key() {
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode("jwtSecretjwtSecretjwtSecretjwtSecretjwtSecretjwtSecretjwtSecretjwtSecretjwtSecretjwtSecretjwtSecretjwtSecretjwtSecretjwtSecret"));
+    private SecretKey key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
     }
 
     public String generateToken(UserDetails userDetails) {
