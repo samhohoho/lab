4db2b1f6379e3a09caabbd8f412a593381e88373
samhohoho <hosamchung998@gmail.com>
2024-09-02 11:16:09 +0800

    add UserController

---
+++ src/main/java/com/samhoho/lab/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.samhoho.lab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserController {
+    @GetMapping("/")
+    public String home(Principal principal) {
+        return principal.getName();
+    }
+}
