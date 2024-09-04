b13afe5137e65285f785d866dac6ae8b4fb7d80d
samhohoho <hosamchung998@gmail.com>
2024-09-04 11:50:24 +0800

    userRepo to extend CrudRepo

--- src/main/java/com/samhoho/lab/repositories/JwtUserRepository.java
+++ src/main/java/com/samhoho/lab/repositories/JwtUserRepository.java
@@ -1,13 +1,13 @@
 package com.samhoho.lab.repositories;
 
 import com.samhoho.lab.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface JwtUserRepository extends JpaRepository<User, Integer> {
+public interface JwtUserRepository extends CrudRepository<User, Long> {
     List<User> findAll();
     User findUserByEmail(String email);
 }
