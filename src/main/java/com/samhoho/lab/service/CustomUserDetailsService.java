a1ba83e7314ee6cade15bb4c47cbfb66e1c61ade
samhohoho <hosamchung998@gmail.com>
2024-09-10 22:34:35 +0800

    modify User type to optional

--- src/main/java/com/samhoho/lab/service/CustomUserDetailsService.java
+++ src/main/java/com/samhoho/lab/service/CustomUserDetailsService.java
@@ -16,8 +16,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email);
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
         return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
 }
