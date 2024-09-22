d207f1d87a66db5a8e34173b37b49183f4ab75f6
samhohoho <hosamchung998@gmail.com>
2024-09-22 23:40:57 +0800

    add POST /cart endpoint

--- src/main/java/com/samhoho/lab/controller/UserController.java
+++ src/main/java/com/samhoho/lab/controller/UserController.java
@@ -1,14 +1,33 @@
 package com.samhoho.lab.controller;
 
+import com.samhoho.lab.service.CartService;
+import com.samhoho.lab.viewmodel.CartItemVm;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 public class UserController {
+    private final CartService cartService;
+
+    public UserController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
     @GetMapping("/")
     public String home(Principal principal) {
         return principal.getName();
+    }
+
+    @PostMapping("/cart")
+    public ResponseEntity<List<CartItemVm>> cart(@RequestBody List<CartItemVm> cartItemVms) {
+        cartService.addToCart(cartItemVms);
+        return new ResponseEntity<>(cartItemVms, HttpStatus.CREATED);
     }
 }
