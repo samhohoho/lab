693b7792f5d2438c1ebca19cb026d80f5a75d02d
samhohoho <hosamchung998@gmail.com>
2024-09-23 20:09:15 +0800

    security config rm unwanted codes

--- src/main/java/com/samhoho/lab/config/WebSecurityConfig.java
+++ src/main/java/com/samhoho/lab/config/WebSecurityConfig.java
@@ -62,22 +62,4 @@ public class WebSecurityConfig {
 
         return providerManager;
     }
-
-//    @Bean
-//    UserDetailsManager users(DataSource dataSource) {
-//        UserDetails user = User.builder()
-//                .username("user")
-//                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
-//                .roles("USER")
-//                .build();
-//        UserDetails admin = User.builder()
-//                .username("admin")
-//                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
-//                .roles("USER", "ADMIN")
-//                .build();
-//        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-//        users.createUser(user);
-//        users.createUser(admin);
-//        return users;
-//    }
 }
